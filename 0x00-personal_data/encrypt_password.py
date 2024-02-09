29d6636850fbe28114f1788bab3ba7d7f909847b
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-02-09 08:37:17 +0300

    encrypt passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import bcrypt
+
+def hash_password(password: str)-> bytes:
+    """ password hasher """
+    salt = bcrypt.gensalt()
+    enc_pass = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return enc_pass
+
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checkes a password's validity """
+    return bcrypt.checkpw(password.encode(), hashed_password)
