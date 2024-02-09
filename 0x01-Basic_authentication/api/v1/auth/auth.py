39e5ed9fec80f88376f717310332e6a74fd803d8
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-02-09 10:26:17 +0300

    submit task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,11 +13,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ checks if authentication is required"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """ checks header for authrization """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ checks the user """
         return None
