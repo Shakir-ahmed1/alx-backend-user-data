5575dcfc8c04857d77040a7d5111acc38d9b9e25
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-07 12:50:41 +0300

    submit code

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,9 +33,14 @@ def before_every_request():
         return
     if not auth.require_auth(request.path,
                              ['/api/v1/status/',
-                              '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+                              '/api/v1/unauthorized/',
+                              '/api/v1/forbidden/',
+                              '/api/v1/auth_session/login/']):
         return
-    if auth.authorization_header(request) is None:
+
+    if auth.authorization_header(request
+                                 ) is None and auth.session_cookie(request
+                                                                   ) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
