d8c1781577c5ebf08aec115b67ef3ad2785ee884
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-07 10:24:15 +0300

    submit code

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" authrization module """
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ authenitcation class"""
+    def __init__(self) -> None:
+        """ intializations """
+        pass
