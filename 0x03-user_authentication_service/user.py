1363a61afccc7b32a070056c5c7df8e173b981be
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-07 17:08:37 +0000

    adding documentation

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,7 +8,8 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = "users"
+    """ a user model"""
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
