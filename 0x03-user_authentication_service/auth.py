a5998b24945c605a3dd3251874abff995d3e2f23
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-08 01:49:32 +0300

    reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,8 +73,7 @@ class Auth:
             search = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
             self._db.update_user(search.id, reset_token=reset_token)
-            return reset_token            
+            return reset_token
 
         except NoResultFound:
             raise ValueError
-        
