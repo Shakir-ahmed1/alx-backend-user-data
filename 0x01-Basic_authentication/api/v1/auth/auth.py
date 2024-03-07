f94c7c955936818d184f5ce02bc61445b8596458
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-07 10:09:07 +0300

    adding request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,7 @@ class Auth:
         """ checks header for authrization """
         if request is None or 'Authorization' not in request.headers:
             return None
-        return request["Authorization"]
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ checks the user """
