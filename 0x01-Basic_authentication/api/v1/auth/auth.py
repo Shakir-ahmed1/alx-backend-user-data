1aa40378e8e0fd068c75e923659c07a70689fc80
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-07 09:43:03 +0300

    slash tolerant

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,7 +3,6 @@
 from flask import request
 from typing import TypeVar, List
 
-
 class Auth:
     """ authenitcation class"""
     def __init__(self) -> None:
@@ -12,18 +11,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ checks if authentication is required"""
-
         if not path or not excluded_paths:
             return True
         sh = path.rstrip('/') + '/'
-        if sh in excluded_paths:
+        if sh in excluded_paths or path.rstrip('/') in excluded_paths:
             return False
         else:
             return True
 
     def authorization_header(self, request=None) -> str:
         """ checks header for authrization """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request["Authorization"]
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ checks the user """
