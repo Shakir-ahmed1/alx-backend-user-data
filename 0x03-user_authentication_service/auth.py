b330586261fda69b9e0152e610eb48a83089024c
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-08 00:48:33 +0300

    submit code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,7 +16,6 @@ def _hash_password(password: str) -> bytes:
 class Auth:
     """Auth class to interact with the authentication database.
     """
-
     def __init__(self):
         """ initialize the class"""
         self._db = DB()
@@ -28,3 +27,12 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+        
+    def valid_login(self, email: str, password: str) -> bool:
+        """ check if the log in is valid """
+        try:
+            search = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), search.hashed_password)
+
+        except NoResultFound:
+            return False
