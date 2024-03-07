daba3539402554c5c6d83169a558fbc96569d932
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-07 17:45:08 +0000

    submit code, find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
+from typing import Dict, Any
 
 
 class DB:
@@ -38,7 +39,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs: Dict[str, Any]) -> User:
         """ find users by filters"""
         filter = self._session.query(User).filter_by(**kwargs)
         if filter.first() is None:
