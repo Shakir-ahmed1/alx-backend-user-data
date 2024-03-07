53fccebb7da132801cf7a8c5bcb511fdeaf860e2
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-07 12:12:18 +0300

    session authentication class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" session authrization module """
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ session authenitcation class"""
+    def __init__(self) -> None:
+        """ intializations """
+        pass
