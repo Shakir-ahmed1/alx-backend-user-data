6266a1d9c501cfc59b01b901d3ee2897f52be5c6
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-08 00:49:24 +0300

    submit code checkpw

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,12 +27,13 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-        
+
     def valid_login(self, email: str, password: str) -> bool:
         """ check if the log in is valid """
         try:
             search = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), search.hashed_password)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  search.hashed_password)
 
         except NoResultFound:
             return False
