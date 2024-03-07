d1c43433ab60ee7efeb2e0233c8e0b1984045d25
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-07 10:11:13 +0300

    adding request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,17 +17,22 @@ if os.getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_every_request():
+    """ excute before every request """
     if auth is None:
         return
-    if not auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+    if not auth.require_auth(request.path,
+                             ['/api/v1/status/',
+                              '/api/v1/unauthorized/', '/api/v1/forbidden/']):
         return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
